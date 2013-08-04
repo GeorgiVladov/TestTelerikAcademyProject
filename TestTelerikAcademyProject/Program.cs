4e9bb252fa91db775ad8068c10615d14913b29f7
GeorgiVladov <gvvladov@gmail.com>
2013-08-04 18:47:35 +0300

    test project

---
+++ TestTelerikAcademyProject/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace TestTelerikAcademyProject
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			IWebDriver driver = new FirefoxDriver();
+			
+			
+		}
+	}
+}
